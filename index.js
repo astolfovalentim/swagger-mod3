2df0bb17b2324552f846b9f77a03f6cf124c3b23
astolfovalentim <astolfom@gmail.com>
2022-04-25 20:40:13 -0300

    .env

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
-const express = require('express');
-const cors = require('cors');
-const port = 3000;
+const express = require("express");
+require("dotenv").config();
+const cors = require("cors");
+const port = process.env.PORT || 3000;
+
 const app = express();
-const paletas = require('./src/routes/paletas.routes');
-const connectToDatabase = require('./src/database/database');
+const paletas = require("./src/routes/paletas.routes");
+const connectToDatabase = require("./src/database/database");
 
 app.use(cors());
 app.use(express.json());
 
 connectToDatabase();
 
-app.use('/paletas', paletas);
+app.use("/paletas", paletas);
 
 app.listen(port, () => {
-  console.log(`Servidor rodando em http://localhost:${port}`);
+  console.log(`Servidor rodando na porta ${port}`);
 });
